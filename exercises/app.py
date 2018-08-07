71c11addd7a59db9f72ec0a1b35f7b1c0e22291d
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 16:41:59 +0300

    hi

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
-
+    music=["the paz band","pink floyd","theangelcy"]
+    return render_template("index.html",music=music)
+ 
 if __name__ == '__main__':
    app.run(debug = True)
